fe59f8a0a240c3188116c366c1c4feb11b134db9
fan <hebofan@126.com>
2020-07-31 11:25:36 +0800

    update case

--- test_mywifi.py
+++ test_mywifi.py
@@ -5,16 +5,17 @@
 
 
 from time import sleep
-from utils.logUtil import Log
+
 # from test.common.browser import Browser
 from selenium import webdriver
-from page.mywifi_page import MywifiPage
+
+from config.globalparam import CONFIG_FILE
 from page.login_page import LoginPage
+from page.mywifi_page import MywifiPage
+from utils.logUtil import Log
+from utils.yamlReaderUtil import YamlConfig
 
 logger = Log()
-
-
-# assertion = YamlConfig(CONFIG_FILE).get("assertion")
 
 
 class TestMywifi(object):
@@ -23,37 +24,39 @@ class TestMywifi(object):
     """
 
     def test_ssid_passwd(self):
-        url = "http://192.168.3.1"  #路由器管理IP
-        passwd = "12345678"         #登录密码
-        ssid2g = "hilink_test"
-        ssid5g = "hilink_test_5g"
-        passwd2g = "012345678901234567890123456789012345678901234567890123456789012"
-        passwd5g = "012345678901234567890123456789012345678901234567890123456789012"
-        assertion = "华为路由WS5200 增强版"
+        RC = YamlConfig(CONFIG_FILE).get("run_config")
+        url = RC.get("url")
+        wl = YamlConfig(CONFIG_FILE).get("web_login")
+        wificonf = YamlConfig(CONFIG_FILE).get("my_wifi")
+        assertion = YamlConfig(CONFIG_FILE).get("assertion")
 
         # +++++++++++++++++++++++++++++登陆设备+++++++++++++++++++
         driver = webdriver.Chrome()
         driver.maximize_window()
         page = LoginPage(driver)
         page.get(url)
-        page.passwd_input.send_keys(passwd)
+        page.passwd_input.send_keys(wl["password"])
         page.login_button.click()
-        assert driver.title == assertion
+        assert driver.title == assertion.get("browser_title")
         logger.info("页面标题是：%s" % (driver.title))
-        # +++++++++++++++++++++++++++配置WIFI++++++++++++++++
+        # +++++++++++++++++++++++++++配置WIFI+++++++++++++++
+        logger.info("++++++++++++++++++test my_wifi START++++++++++++++++++++++++")
         page = MywifiPage(driver)
         page.main_menu_button.click()
         page.name2g_input.clear()
-        page.name2g_input.send_keys(ssid2g)
-        page.passwd2g_input.send_keys(passwd2g)
+        page.name2g_input.send_keys(wificonf["ssid2g"])
+        page.passwd2g_input.send_keys(wificonf["passwd2g"])
         # page.check_old_passwd.send_keys(passwd)
         page.name5g_input.clear()
-        page.name5g_input.send_keys(ssid5g)
-        page.passwd5g_input.send_keys(passwd5g)
+        page.name5g_input.send_keys(wificonf["ssid5g"])
+        page.passwd5g_input.send_keys(wificonf["passwd5g"])
         page.save_button.click()
-        sleep(10)
+        sleep(5)
         driver.close()
         print("2.4g和5g无线配置完成，终端连接确认配置是否生效")
+        logger.info("wifi配置如下:\n2.4G SSID: %s,\n2.4G PASSWORD: %s,\n5G SSID: %s,\n5G PASSWORD: %s " %
+                    (wificonf["ssid2g"],wificonf["passwd2g"],wificonf["ssid5g"],wificonf["passwd5g"]))
+
 
 if __name__ == "__main__":
     TestMywifi().test_ssid_passwd()
